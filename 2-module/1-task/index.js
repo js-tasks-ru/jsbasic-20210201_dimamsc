7788cdc26358c3fc4daca6583277208ce5a90622
Dmitrii Smirnov <65165825+DmitrySmirnovMSC@users.noreply.github.com>
2021-03-09 23:32:51 +0300

    Возврат изменений 1-module/4-task/index.js, 2-module/1-task/index.js

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,31 @@
+let salaries = {
+    John: 1000,
+    Ann: 1600,
+    Pete: 1300,
+    month: "December",
+    currency: "USD",
+    isPayed: false,
+    result1: 7 / 0,
+    result2: -7 / 0,
+    other: NaN,
+};
+
+let sum = 0;
+let typeOfValue;
+let valueInSalaries;
+
 function sumSalary(salaries) {
-  // ваш код...
+    for (valueOfSalaries in salaries) {
+        typeOfValue = typeof salaries[valueOfSalaries];
+        valueInSalaries = salaries[valueOfSalaries];
+        if (
+            valueInSalaries == Number.POSITIVE_INFINITY ||
+            valueInSalaries == Number.NEGATIVE_INFINITY
+        ) {} else if (isNaN(valueInSalaries) == true && typeOfValue == "number") {} else if (typeOfValue == "number") {
+            sum = sum + valueInSalaries;
+        } else {}
+    }
+    console.log(sum);
 }
+
+sumSalary(salaries);
